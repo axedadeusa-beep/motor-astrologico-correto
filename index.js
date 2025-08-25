c1d6d77b6ee730946fb0f80ea43b51b77d805fa3
axedadeusa-beep <axedadeusa@gmail.com>
2025-08-25 12:27:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const sweph = require('sweph');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
 
 sweph.set_ephe_path(__dirname + '/node_modules/sweph/ephe');
 
